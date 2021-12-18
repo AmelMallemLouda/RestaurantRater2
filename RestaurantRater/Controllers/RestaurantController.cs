694c9b5f0c52eb0681354d001cca95947fbea16a
Amel MallemLouda <amelmalleml2016@gmail.com>
2021-12-18 18:55:27 -0500

    IndexView

---
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -0,0 +1,24 @@
+﻿using RestaurantRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantRater.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        //we need to do is access our stored Restaurants.We can do this by adding a RestaurantDbContext instance to our class.
+        //    We'll be adding one to the class as a field so all future methods can reference the same one.
+
+        private RestaurantDbContext _db = new RestaurantDbContext();
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+
+            //We're reaching into the _db field, calling the Restaurants property which is the DbSet we created, and then converting it to a List with the .ToList() method call. That list is then passed into the View method which will then pass it to the View as the Model.
+            return View(_db.Resaturants.ToList());
+        }
+    }
+}
---
+++ RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+    }
+
+    public class RestaurantDbContext:DbContext
+    {
+        //DbSet is a collection of entities that can be queried from the database. it will act as a way to access all Restaurant objects that we set in our database
+        //Entity is word used to describe objects that are being stored into the database
+        //Connection string is used as a connection between our class and the database
+        public DbSet<Restaurant> Resaturants { get; set; }
+    }
+}
