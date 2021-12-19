d76b3b3bcc23e9c75f515a03573d626ae79893b3
Amel MallemLouda <amelmalleml2016@gmail.com>
2021-12-18 19:17:42 -0500

    Create

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -20,5 +20,31 @@ namespace RestaurantRater.Controllers
             //We're reaching into the _db field, calling the Restaurants property which is the DbSet we created, and then converting it to a List with the .ToList() method call. That list is then passed into the View method which will then pass it to the View as the Model.
             return View(_db.Resaturants.ToList());
         }
+
+        // Get : Restaurant/ Create
+        //A GET request is set to just get the information
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //Post Restaurant Rater/ Create
+        //HttpPost is an annotation that specifies that this method is going to be a POST method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //ValidateAntiForgeryToken is another annotation that allows the server to match up with the AntiForgeryToken that we have set in the Create form.
+        public ActionResult Create (Restaurant restaurant)
+        {
+            //ModelState.IsValid is going to check the model given to the method.So if restaurant (the parameter) has any requirements that are not met, it would check the ModelState and IsValid would be false and not save the changes
+            //If the ModelState isn't valid, we see it return to the View() but we are also passing restaurant back to the view. We do that by putting return View(restaurant); the same way we passed the _db.Restaurants.ToList() in the Index method.
+           //If the ModelState.IsValid is true, then we go in and add and save the changes and then return a new method called RedirectToAction("Index"); which takes the string name of another action(the Index method in this case) and redirects the code to that action.So here we're seeing the return kick us over to the Index action which takes us to the Index view.
+            if (ModelState.IsValid)
+            {
+                _db.Resaturants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
     }
 }
